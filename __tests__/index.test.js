0e64d3127673dc906f502f0c52f9f7189f1e6eae
Takuto Komazaki <komazarari@gmail.com>
2020-09-28 12:35:01 +0900

    init

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+const fizzbuzz = require("../index");
+
+describe("fizzbuzz", () => {
+    it("returns FizzBuzz when value is divisible by 15", () => {
+        expect(fizzbuzz(30)).toStrictEqual("FizzBuzz");
+    });
+    it("returns Fizz when value is divisible by 3", () => {
+        expect(fizzbuzz(9)).toStrictEqual("Fizz");
+    });
+    it("returns Buzz when value is divisible by 5", () => {
+        expect(fizzbuzz(10)).toStrictEqual("Buzz");
+    });
+    it("returns the value when value is not divisible by 3 or 5", () => {
+        expect(fizzbuzz(8)).toStrictEqual("8");
+    });
+});
